ec7e560ea19f0044ed9648f91716539d498476ba
Christos Daglaroglou <christos.daglaroglou@gmail.com>
2025-08-02 20:34:39 +0300

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=DVG Kernel by @daglaroglou
+kernel.string=DΛG Kernel by @daglaroglou
 do.devicecheck=0
 do.modules=0
 do.systemless=0
